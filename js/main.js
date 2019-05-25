2b7f8ab6d6e7097cc1bb26aa86c956d819a7aaad
Tobiasz Maj <tobiaszmaj1@gmail.com>
2019-05-25 02:40:41 +0200

    Update about & skills pages

--- js/main.js
+++ js/main.js
@@ -1,5 +1,4 @@
 // Preloader
-
 const curry = f => (...args) =>
     args.length >= f.length ?
     f(...args) :
@@ -20,23 +19,3 @@ setTimeout(function () {
         this.classList.remove('preloader-hiding');
     });
 }, 4000);
-
-// Form
-(function ($) {
-    function floatLabel(inputType) {
-        $(inputType).each(function () {
-            var $this = $(this);
-            // on focus add cladd active to label
-            $this.focus(function () {
-                $this.next().addClass("active");
-            });
-            //on blur check field and remove class if needed
-            $this.blur(function () {
-                if ($this.val() === '' || $this.val() === 'blank') {
-                    $this.next().removeClass();
-                }
-            });
-        });
-    }
-    floatLabel(".floatLabel");
-})(jQuery);
