a40b8bd9d3ab66b8ae8798774196ccd91c07878d
Tobiasz Maj <tobiaszmaj1@gmail.com>
2019-06-14 03:05:28 +0200

    Mobile nav

--- js/main.js
+++ js/main.js
@@ -19,3 +19,33 @@ setTimeout(function () {
         this.classList.remove('preloader-hiding');
     });
 }, 4000);
+
+// Mobile Menu
+let burger = document.getElementById('burger'),
+    nav = document.getElementById('mobile-nav'),
+    slowmo = document.getElementById('slowmo');
+
+burger.addEventListener('click', function (e) {
+    this.classList.toggle('is-open');
+    nav.classList.toggle('is-open');
+});
+
+slowmo.addEventListener('click', function (e) {
+    this.classList.toggle('is-slowmo');
+});
+
+/* Onload demo - dirty timeout */
+let clickEvent = new Event('click');
+
+window.addEventListener('load', function (e) {
+    slowmo.dispatchEvent(clickEvent);
+    burger.dispatchEvent(clickEvent);
+
+    setTimeout(function () {
+        burger.dispatchEvent(clickEvent);
+
+        setTimeout(function () {
+            slowmo.dispatchEvent(clickEvent);
+        }, 3500);
+    }, 5500);
+});
